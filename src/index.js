25269b8177d8094de253b0064c6b91d1724e660a
Yui-Terai <47691090+Yui-Terai@users.noreply.github.com>
2019-07-27 12:01:51 +0800

    Reducer

--- src/index.js
+++ src/index.js
@@ -21,6 +21,11 @@ const reducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_ITEM":
       return [...state, action.item];
+    case "DELETE_ITEM":
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ];
     default:
       return state;
   }
